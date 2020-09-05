0a1d0d969dd799b96fe67d42852f839a7eec8517
Ellian Abe <ellian_abe@outlook.com>
2020-09-04 22:45:23 -0300

    Atualizando

---
+++ src/main/java/br/uniso/Student.java
@@ -0,0 +1,32 @@
+package br.uniso;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private int ra;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getRa() {
+        return ra;
+    }
+
+    public void setRa(int ra) {
+        this.ra = ra;
+    }
+}
--- src/main/java/br/uniso/UnisoController.java
+++ src/main/java/br/uniso/UnisoController.java
@@ -1,20 +1,24 @@
 package br.uniso;
 
-// eh a nossa classe de controller
-// ela pega a requisicao http vinda do browser
-// e processa pra devolver uma resposta
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 
 @RestController
 public class UnisoController {
+
+    ArrayList<Student> students = new ArrayList<>();
+    @PostMapping("/createStudent")
+    public boolean createStudent(@RequestBody Student student){
+        students.add(student);
+        return true;
+    }
 
     @GetMapping("/hello")
     public String helloWorld(@RequestParam("name") String a, @RequestParam("age") String b){
         return "Hello " + a + " Idade: " + b;
     }
+
+    @GetMapping("/students")
+    public ArrayList<Student> getStudent() { return students;   }
 }
